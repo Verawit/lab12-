47974e4b048124c7467a8fc6eceec474c2c706ea
Verawit <Verawit2548@gmail.com>
2024-01-05 13:08:37 +0700

    lab12_1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int n){
+    int N;
+    if(n == 0){
+        N = 0;
+    }
+    if(n == 1){
+        N = 1;
+    }
+    if(n > 1){
+        N = fibonacci(n-1) + fibonacci(n-2);
+    }
+    return N;
 }
